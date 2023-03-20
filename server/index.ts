e30854221a68640d7a6f244fdca368cf7a92928f
Kaic Bastidas <kaic.bastidas@gympass.com>
2023-03-20 11:39:11 +0000

    first setup

---
+++ server/index.ts
@@ -0,0 +1,44 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import teste from 'apollo-graphiql-playground'
+
+console.log('teste:', teste)
+
+const typeDefs = /* GraphQL */`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
